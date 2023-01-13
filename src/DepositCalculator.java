b6ada94c4dafa9bcc69324e68afe97864d30ae7e
kozjulia <120213616+kozjulia@users.noreply.github.com>
2023-01-13 17:55:36 +0300

    Проведено ревью
    Подписала замечания в комментах к коду. Программа рабочая - молодец!

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
-
+//поясняющие комментарии к методам - молодец!
 public class DepositCalculator { // Считаем процентную ставку
-    double calculateComplexPercent(double a, double y, int d) { 
+    double calculateComplexPercent(double a, double y, int d) {  // тут можно лучше - переменным нужно давать точные, «говорящие» имена
         //вычисляеv комплексный процент по заданному вкладу, годовой процентной ставке и сроку вклада.
         double pay = a * Math.pow((1 + y / 12), 12 * d);
-        return rand(pay, 2);
+        return rand(pay, 2);  
     }
 
-    double calculateSimplePercent(double Amount, double yearRate, int depositPeriod) { 
+    double calculateSimplePercent(double Amount, double yearRate, int depositPeriod) { // тут можно лучше - названия переменных пишутся в lowerCamelCase
         //вычисляем простой процент по заданному вкладу, годовой процентной ставке и сроку вклада.
         return rand(Amount + Amount * yearRate * depositPeriod, 2);
     }
 
-    double rand(double value, int places) {
+    double rand(double value, int places) { // и здесь аналогично можно лучше - методам нужно давать точные, «говорящие» имена 
         //округляем данное значение до указанного количества знаков после запятой.
-        double scale = Math.pow
+        double scale = Math.pow        
                 (10, places);
+        // ниже и выше строки в коде лучше строки не переносить - длина строки может быть 120 символов
         return Math.round(value * scale)
                 / scale;
     }
 
     void viewMenuAndCalculatingIncome() { //Узнаём входные даные и высчитываем сумму по окончанию вклада
         int period, action;
+        // тут можно лучше - между объявлением переменных и остальным кодом ставится пустая строка
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
         int amount = scanner.nextInt();
